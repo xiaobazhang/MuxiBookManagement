f916b1953d79624616ace6305be3d5e738f16642
suli <suli@meili-inc.com>
2017-06-24 15:57:51 +0800

    添加了代码

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "I hate flask!"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///path/to/data.sqlite"  # 系统相应替换
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////path/to/data.sqlite"  # 系统相应替换
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/suli/ClionProjects/MuxiBookManagement/data.sqlite"  # 系统相应替换
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['WHOOSH_BASE'] = "search.db"
 app.config['MAX_SEARCH_RESULTS'] = 100  # 最大加载5个搜索结果
